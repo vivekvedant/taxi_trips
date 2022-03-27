db76df7918ae37726f33b221927502f4f07ab741
= <vivekvedant86@gmail.com>
2022-03-27 15:19:30 +0530

    fixed form issue

--- src/flask_app/app.py
+++ src/flask_app/app.py
@@ -1,7 +1,8 @@
 from flask import Flask, render_template
 import os
 import pickle
-import pandas as pd
+# import pandas as pd
+import json
 
 template_dir =  os.path.join(os.path.dirname(os.path.abspath(__file__)),"templates")
 
@@ -14,8 +15,11 @@ server = Flask(__name__,static_folder = static_dir,template_folder = template_di
 @server.route("/",methods = ['GET'])
 def index():
     # get the location zone name
-    read_zone_pickle_file = pickle.load(open("../pickle_files/zone.pickle","rb"))
-    return render_template("index.html",zone_list=read_zone_pickle_file)
+    # read_zone_pickle_file = pickle.load(open("../pickle_files/zone.pickle","rb"))
+    with open("../../conf/location_id.json", "r") as outfile:
+        zone_info =  json.load(outfile)
+    #  = json.loads(open(,"rb"))
+    return render_template("index.html",zone_list=zone_info)
 
 
 
